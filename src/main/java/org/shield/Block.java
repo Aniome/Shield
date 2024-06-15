00d4264406cb1a1f748e029ef305eb4df0903c6a
Aniome <vaganov.kostya2000@yandex.ru>
2024-06-15 21:40:27 +0500

    add Block class

--- src/main/java/org/shield/Block.java
+++ src/main/java/org/shield/Block.java
@@ -1,5 +1,26 @@
 package org.shield;
 
+import java.util.Date;
+import java.util.HashMap;
+
 public class Block {
     int index;
+    Date timestamp;
+    Date dateOfManufacture;
+    int proof;
+    String manufacturer;
+    String nameOfProduct;
+    String previousHash;
+    String productDescription;
+
+    public Block(int index, Date timestamp, int proof, Date dateOfManufacture, HashMap<String, String> info){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.proof = proof;
+        previousHash = info.get("previousHash");
+        this.dateOfManufacture = dateOfManufacture;
+        manufacturer = info.get("manufacturer");
+        nameOfProduct = info.get("nameOfProduct");
+        productDescription = info.get("productDescription");
+    }
 }
