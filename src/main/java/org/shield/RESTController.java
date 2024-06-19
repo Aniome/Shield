d3627f5bb1219806c1d2b1c86b4bdc67d7893d5a
Aniome <vaganov.kostya2000@yandex.ru>
2024-06-19 21:47:07 +0500

    initial create html file

--- src/main/java/org/shield/RESTController.java
+++ src/main/java/org/shield/RESTController.java
@@ -1,14 +1,20 @@
 package org.shield;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/")
 public class RESTController {
-    @GetMapping("/hello")
+    @GetMapping("/hello-world")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/helloAll")
+    public String helloAll() {
+        return "Hello World everybody";
     }
 }
--- src/main/java/org/shield/SecurityConfig.java
+++ src/main/java/org/shield/SecurityConfig.java
@@ -1,16 +1,20 @@
 package org.shield;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationEventPublisher;
-import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -19,14 +23,26 @@ public class SecurityConfig {
     @Bean
     @ConditionalOnMissingBean(UserDetailsService.class)
     InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        String generatedPassword = "{noop}123456";
-        return new InMemoryUserDetailsManager(User.withUsername("user")
-                .password(generatedPassword).roles("USER").build());
+        String generatedPassword = "{noop}user";
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+        inMemoryUserDetailsManager.createUser(User.withUsername("user")
+                .password(generatedPassword).roles("user").build());
+        return inMemoryUserDetailsManager;
     }
 
     @Bean
-    @ConditionalOnMissingBean(AuthenticationEventPublisher.class)
-    DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher delegate) {
-        return new DefaultAuthenticationEventPublisher(delegate);
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/helloAll").permitAll()
+                        .requestMatchers("/hello").permitAll()
+                        .requestMatchers("/hello-world").hasRole("user"))
+                .formLogin(form -> form.defaultSuccessUrl("/hello", true));
+        return http.build();
     }
+//
+//    @Bean
+//    @ConditionalOnMissingBean(AuthenticationEventPublisher.class)
+//    DefaultAuthenticationEventPublisher defaultAuthenticationEventPublisher(ApplicationEventPublisher delegate) {
+//        return new DefaultAuthenticationEventPublisher(delegate);
+//    }
 }
