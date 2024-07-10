39dd2075ab938495ab50033bc61e5c97fa7c3a32
Aniome <vaganov.kostya2000@yandex.ru>
2024-07-10 20:25:54 +0500

    add custom 403,error page

--- src/main/java/org/shield/controllers/MainController.java
+++ src/main/java/org/shield/controllers/MainController.java
@@ -10,9 +10,4 @@ public class MainController {
     public String login() {
         return "login";
     }
-
-    @ExceptionHandler
-    public String error() {
-        return "error";
-    }
 }
--- src/main/java/org/shield/controllers/ShieldErrorController.java
+++ src/main/java/org/shield/controllers/ShieldErrorController.java
@@ -5,26 +5,27 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ShieldErrorController implements ErrorController {
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
 
             if(statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "404";
+                return "errors/404";
             } else if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "403";
+                return "errors/403";
             } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
                 //500
-                return "error";
+                return "errors/error";
             }
         }
-        return "error";
+        model.addAttribute("error", "Error - unknown error");
+        return "errors/error";
     }
 }
